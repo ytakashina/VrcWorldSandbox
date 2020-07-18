5ae870ef178be4a94364b790844f7088e4764a76
allergy_zamurai <441121111@users.noreply.github.com>
2020-07-18 22:44:29 +0900

    ボタン追加
    ・得点をリセットするボタン
    ・ボール位置をリセットするボタン

---
+++ Assets/UdonSharp/Scripts/ResetBallButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UdonSharp;
+using UnityEngine;
+
+public class ResetBallButton : UdonSharpBehaviour
+{
+    [SerializeField]
+    public GameObject ball;
+
+    [SerializeField]
+    public Vector3 defaultBallPosition;
+
+    public void Reset()
+    {
+        ball.transform.position = defaultBallPosition;
+        ball.GetComponent<Rigidbody>().velocity = new Vector3(0, 10, 0);
+    }
+
+    private void OnMouseDown()
+    {
+        Reset();
+    }
+    public override void Interact()
+    {
+        Reset();
+    }
+}
+
---
+++ Assets/UdonSharp/Scripts/ResetPointButton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UdonSharp;
+using UnityEngine;
+
+public class ResetPointButton : UdonSharpBehaviour
+{
+    [SerializeField]
+    public UdonSharpBehaviour[] goalBoards;
+
+    public void Reset()
+    {
+        foreach(var goalBoard in goalBoards)
+        {
+            goalBoard.SetProgramVariable("_score", 0);
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        Reset();
+    }
+    public override void Interact()
+    {
+        Reset();
+    }
+}
+
